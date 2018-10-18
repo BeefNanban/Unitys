9242ef9d851ffbee880711b2792f4119056fa00a
Nanban <dminpo@gmail.com>
2018-10-18 15:30:08 +0900

    net
    ネット

---
+++ 仮/Assets/Launchers.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launchers : Photon.PunBehaviour {
+
+    public string ObjectName;
+
+	// Use this for initialization
+	void Start () {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+        PhotonNetwork.sendRate = 30;
+	}
+	
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("ログイン成功");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public void OnPhotonRandomJoinFailed()
+    {
+        Debug.Log("ログイン失敗");
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    public override void OnConnectedToPhoton()
+    {
+        Debug.Log("接続開始");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("ルームイン");
+    }
+    // Update is called once per frame
+    void OnGUI () {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+	}
+}
